f41c7ef714fec7f2b443641ba480d662e7e69158
Jorge_Barrios <jorge.barriosn@usm.cl>
2022-07-11 18:16:12 -0400

    Cambio en as rutas y limite del servidor

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require("fs");
 const app = express();
 
-mongoose.connect("mongodb+srv://jorge:1234@cluster0.yj9r6pq.mongodb.net/?retryWrites=true&w=majority", (req,res)=>{
-    console.log("conectado a la base de datos")
-})
-app.get("/enviarVideo", async (req,res)=>{
-    const range = req.headers.range;
-    if (!range) {
-        res.status(400).send("Requires Range header");
-    }
-    const videoPath= "4K.mp4";
-    const videoSize= fs.statSync("4K.mp4").size;
-    const CHUNK_SIZE = 10 ** 6;
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-    const contentLength = end - start + 1;
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4",
-    };
-    res.writeHead(206,headers);
-    const videoStream = fs.createReadStream(videoPath,{start,end});
-    videoStream.pipe(res);
-})
+app.use(cors());
+app.use(express.json())
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb',extended:true}));
+
+app.get('/',(req,res)=>{
+    res.send('Hola Mundo!');
+});
+app.post('/upload_chunk',(req,res)=>{
+    let timestamp = req.query.timestamp;
+    let type = req.query.type;
+    console.log(req.body.chunkData)
+    res.send(`Chunk recibido:${req.body.chunData}`)
+});
+app.post('/upload_extra_data',(req,res)=>{
+    console.log(req.body)
+    res.send(`Chunk recibido:${req.body}`)
+});
 //Set the port that you want the server to run on
 const port = process.env.PORT || 8080;
 app.listen(port);
