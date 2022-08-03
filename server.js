9f3343d188e3310c405c4491874e078fb2794a11
Jorge_Barrios <jorge.barriosn@usm.cl>
2022-08-03 11:21:30 -0400

    Stash para cambios de multer y socket.io

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const http = require('http');
-const server = http.createServer(app)
 const { Server } = require("socket.io");
+const server = http.createServer(app)
 const io = new Server(server);
 const { writeFile } = require('fs');
+const bodyParser = require('body-parser');
+const ffmpeg = require("ffmpeg");
+const tmp = require("tmp");
+const fs = require("fs");
+const multer = require('multer');
+const upload = multer({dest: 'uploads /'}); // los archivos se cargarán en este directorio
 
 io.on("connection", (socket) => {
     socket.on('uploadVideo', (file, callback) => {
@@ -22,24 +28,46 @@ io.on("connection", (socket) => {
 });
 
 app.use(cors());
-const bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: "10mb"}));
 app.use(bodyParser.urlencoded({
-    limit: '10mb',
+    limit: '5mb',
     parameterLimit: 100000,
     extended: true
 }));
+let tmpobj = tmp.fileSync({ postfix: '.webm' })
 
+async function convert_images(video_bytes_array){
+    fs.writeFileSync(tmpobj.name, video_bytes_array);
+    try {
+        const process = new ffmpeg(tmpobj.name);
+        process.then(function (video) {
+            // Callback mode
+            video.fnExtractFrameToJPG('./', { // make sure you defined the directory where you want to save the images
+                frame_rate : 1,
+                number : 10,
+                file_name : 'my_frame_%t_%s'
+            }, function (error, files) {
+                if (!error)
+                    tmpobj.removeCallback();
+            });
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
 app.get('/',(req,res)=>{
     res.send('Hola Mundo!');
 });
+app.post('/upload_chunk',(req,res)=>{
+    let timestamp = req.query.timestamp;
+    let type = req.query.type;
+    console.log(req.body.chunkData)
+    res.send(`Chunk recibido:${req.body.chunkData}`)
+});
+
 app.post('/upload',(req,res)=>{
     console.log(req.body)
-    res.send(`Archivo recibido:${req.body.path}`)
-});
-app.post('/upload_extra_data',(req,res)=>{
-    console.log(req.body)
-    res.send(`Chunk recibido:${req.body}`)
+    res.send(`Respuesta: ${req.body}`)
 });
 
 //Set the port that you want the server to run on
