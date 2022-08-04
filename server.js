4bbd08b6d89a507c5bfa7331af4442cb3f6e0a4d
Jorge_Barrios <jorge.barriosn@usm.cl>
2022-08-04 16:34:42 -0400

    Cambio para leer archivo

--- server.js
+++ server.js
@@ -5,37 +5,43 @@ const http = require('http');
 const server = http.createServer(app)
 const { Server } = require("socket.io");
 const io = new Server(server);
-const { writeFile } = require('fs');
+const bodyParser = require('body-parser');
+const multer = require('multer')({limits:{fieldSize: 2000000000}});
+const fs = require('fs');
+
 
 io.on("connection", (socket) => {
-    socket.on('uploadVideo', (file, callback) => {
-        console.log("xd")
-        require("fs").writeFile("out.mp4",file, 'base64', function(err) {
+    socket.on('upload_Video', (file, callback) => {
+        console.log(file)
+        fs.writeFile("video.mp4",file, 'base64', function(err) {
             console.log(err);
         });
     });
     socket.on('uploadPhoto', (file, callback) => {
-        require("fs").writeFile("out.jpg",file, 'base64', function(err) {
+        fs.writeFile("out.jpg",file, 'base64', function(err) {
             console.log(err);
         });
     });
 });
 
 app.use(cors());
-const bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: "10mb"}));
 app.use(bodyParser.urlencoded({
     limit: '10mb',
     parameterLimit: 100000,
     extended: true
 }));
-
 app.get('/',(req,res)=>{
     res.send('Hola Mundo!');
 });
-app.post('/upload',(req,res)=>{
-    console.log(req.body)
-    res.send(`Archivo recibido:${req.body.path}`)
+app.post('/uploadVideo',multer.single('fileFieldName'),async (req, res) => {
+/*    const buff = Buffer.from(req.body.VideoRN, 'base64');
+    console.log(buff)*/
+    console.log((req.body.VideoRN).length)
+    await fs.writeFile("out.mp4", req.body.VideoRN, 'base64', function (err) {
+        console.log(err)
+    });
+    res.send(`Video recibido`)
 });
 app.post('/upload_extra_data',(req,res)=>{
     console.log(req.body)
